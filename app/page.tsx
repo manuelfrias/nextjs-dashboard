24237d02f1c8e32bcaee3f3885434ac69f849007
manufrias <manufrias@icloud.com>
2024-01-22 15:13:10 -0500

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Page() {
   return (
