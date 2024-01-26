3f0377e8e6e3155c39d308b25e7784eedd379bc2
manufrias <manufrias@icloud.com>
2024-01-26 17:30:56 -0500

    chapter 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function RootLayout({
   children,
 }: {
@@ -10,7 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   );
